e10558e55d6d873a363e6c0f779a950620a13005
szolarisz <thomas@nyirszikszi.hu>
2021-12-01 21:19:41 +0100

    Alapértelmezett oldallal kiegészítve. JSON file és soma JSON üzenet küldéssel kiegészítve

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 4444;
 
 app.get('/', function (req, res) {
      const htmlFile = path.join(__dirname, "views/index.html");
-     console.log(htmlFile);
+     //console.log(htmlFile);
      res.sendFile(htmlFile);
     /*
     Linux,Apple /
@@ -16,8 +16,39 @@ app.get('/', function (req, res) {
    
 });
 
+/*
+  https://expressjs.com/en/api.html#res.sendFile
+  https://install.advancedrestclient.com/install
+*/
+
+app.get('/mycolors', (req, res) => {
+    //res.sendStatus(200);
+    res.sendFile(path.join(__dirname, "data/colors.json"));   
+ });
+
+ app.get('/alap.css', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/alap.css"));   
+ });
+ 
+
+app.get('/colors', (req, res) => {
+   res.json([{
+       'code':'#A0A0A0',
+    'name': 'Szürke'}]);
+   
+});
+
 app.get('/script.js', (req, res) =>{
     res.sendFile(path.join(__dirname,"public/script.js"));
-})
- 
+});
+
+/*app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname,"views/hiba.html"))
+});
+*/
+app.get('*',function (req, res) {
+    res.redirect('/');
+});
+
+
 app.listen(port)
